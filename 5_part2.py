1de15a849ee49ecea0902f4cb5a201c67a802f8e
Benjamin Schubert <brm.schubert@gmail.com>
2018-12-05 08:50:25 -0500

    Major performance improvement

--- 5_part2.py
+++ 5_part2.py
@@ -10,31 +10,26 @@ for letter in "abcdefghijklmnopqrstuvwxyz":
             data = data[0:i] + data[i+1:]
             continue
         i += 1
-    maxlength = 1000000
-    localmax = 0
-    lastlocalmax = 1000000
-    while localmax != lastlocalmax:
-        i = 0
-        while i+1 < len(data):
-            char = data[i]
-            if char.islower():
-                if data[i+1] == char.upper():
-                    data = data[0:i] + data[i+2:]
-                    continue
-            if char.isupper():
-                if data[i+1] == char.lower():
-                    data = data[0:i] + data[i+2:]
-                    continue
-            i += 1
-            if i > localmax:
-                localmax = i
-        if localmax < maxlength:
-            maxlength = localmax
-            lastlocalmax = localmax
-            localmax = 0
+
+    i = 0
+    while i+1 < len(data):
+        char = data[i]
+        if char.islower():
+            if data[i+1] == char.upper():
+                data = data[0:i] + data[i+2:]
+                if i > 0:
+                    i -= 1
+                continue
+        if char.isupper():
+            if data[i+1] == char.lower():
+                data = data[0:i] + data[i+2:]
+                if i > 0:
+                    i -= 1
+                continue
+        i += 1
 
     if len(data) < best_count:
         best_count = len(data)
         best_letter = letter
 
-print("\n{}: {}".format(best_letter, best_count))
+print("{}: {}".format(best_letter, best_count))

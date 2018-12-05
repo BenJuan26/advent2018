5c3785959dd08e608c766df3c61799d236be7647
Benjamin Schubert <brm.schubert@gmail.com>
2018-12-05 08:54:27 -0500

    Cleanup

--- 5.py
+++ 5.py
@@ -2,22 +2,14 @@ from aoc import get_input
 
 data = get_input(5)
 
-
 i = 0
 while i+1 < len(data):
     char = data[i]
-    if char.islower():
-        if data[i+1] == char.upper():
-            data = data[0:i] + data[i+2:]
-            if i > 0:
-                i -= 1
-            continue
-    if char.isupper():
-        if data[i+1] == char.lower():
-            data = data[0:i] + data[i+2:]
-            if i > 0:
-                i -= 1
-            continue
+    if char.islower() and data[i+1] == char.upper() or char.isupper() and data[i+1] == char.lower():
+        data = data[0:i] + data[i+2:]
+        if i > 0:
+            i -= 1
+        continue
     i += 1
 
 print(len(data))
